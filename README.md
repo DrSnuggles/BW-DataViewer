c885019af2bc3eec172d50e15f0322c39a8e30d9
DrSnuggles <snugglesdr@hotmail.com>
2021-06-12 14:52:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Bloodwych Data Viewer
+
+## Why
+
+I did an IFF parser for audio/video a while back. See https://drsnuggles.github.io/IFF/
+
+But there was no viewer for Bloodwych GFX files which are Atari ST raw file format.
+
+Thanks to https://github.com/HoraceAndTheSpider/Bloodwych-68k/wiki i could manage to change that.
+
+## GFX
+
+Thats the new part done here.
+
+## SFX
+
+For playing back sounds i use my IFF/8SVX lib back from 2020.
+
+## License
+Public Domain
---
+++ js/bwgfx.js
@@ -0,0 +1,162 @@
+/*  Bloodwych GFX viewer by DrSnuggles
+    License : Public Domain
+ */
+
+"use strict";
+
+var BWGFX = (function (my) {
+  //
+  // Init
+  //
+  var f = {}; // main object
+  var debug = true;
+  var size;
+
+  //
+  // Private
+  //
+  function log(out) {
+    if (debug) console.log("BWGFX:", out);
+    var dbg_DOM = document.getElementById("info");
+    if (dbg_DOM) dbg_DOM.innerHTML += out +"<br/>";
+  }
+  function xhr(url, cb) {
+    log("xhr: "+ url);
+    var x = new XMLHttpRequest();
+    x.open("GET", url, true);
+    x.responseType = "arraybuffer";
+    x.onload = function() {
+      log("File loaded");
+      if (cb) cb(x.response);
+    }
+    x.send();
+  }
+  function load(url, canv) {
+    log("load: "+ url);
+    xhr(url, function(buf){
+      /*
+      xhr(url.replace('/bw-gfx/','/bw-data/').toLowerCase()+'.colours', function(colours){
+        xhr(url.replace('/bw-gfx/','/bw-data/').toLowerCase()+'.gradeoffset', function(gradeoffset){
+          parse(buf, colours, gradeoffset, canv);
+        });
+      });
+      */
+      parse(buf, null, null, canv);
+    });
+  }
+  function parse(buf, col, grade, canv) {
+    f.data = new Uint8Array(buf); // 5096 bytes
+    f.idx = 0;
+    //f.i8 = new Int8Array(buf);
+    f.col = new Uint8Array(col); // [9, 11, 10, 8, 3, 4, 5, 7]
+    f.grade = new Uint8Array(grade); // 6
+    f.canv = canv;//document.createElement('canvas');
+    f.ctx = f.canv.getContext('2d');
+
+    render(range_width.value);
+  }
+  function render(renderWidth) {
+    if (!f.canv) return // early exit
+    if (renderWidth > f.data.length) return // early exit
+    f.canv.width = renderWidth; // steps = 4
+    f.canv.height = f.data.length / f.canv.width; // todo: round up to 4 ???
+    var imgData = f.ctx.createImageData(f.canv.width,f.canv.height)
+
+    // colors
+    // https://github.com/HoraceAndTheSpider/Bloodwych-68k/wiki/Game-Palette
+    //var cols = ['000','444','666','888','AAA','292','1C1','00E','48E','821','B31','E96','D00','FD0','EEE','C08']
+    var cols = ['000000','404040','606060','808080','A0A0A0','209020','10C010','0000E0','4080E0','802010','B03010','E09060','D00000','F0D000','E0E0E0','C00080']
+    
+    // grade
+    // https://github.com/HoraceAndTheSpider/Bloodwych-68k/wiki/Colouring-Large-Monsters
+    
+    // data
+    // https://github.com/HoraceAndTheSpider/Bloodwych-68k/wiki/Atari-ST-Raw-Data-Format
+    // Atari 320x200, 16 color, you always have one quadword for one pixelblock of 16 pixels
+    // Each word describes one plane and got 16 bits for the 16 pixels
+    // 16 colors = 4 planes = quadword, word = 2bytes -> 4width x 4height x 4planes = 64 bits = 8 bytes OK
+    f.pixelblocksTotal = f.data.length/8;
+    //log(f.pixelblocksTotal);
+    
+    var pixelNum = 0;
+    for (var i = 0; i < f.pixelblocksTotal; i++) {
+      var p = readPixelBlock(f);
+    
+      for (var y = 0; y < 4; y++) {
+        for (var x = 0; x < 4; x++) {
+          var pixelColorNum = get_atari_pixelcolor(p,y*4+x);
+          var pixelCol = cols[pixelColorNum];
+          //console.log('pixelBlock '+i, y*4+x, pixelColorNum, cols[pixelColorNum]);
+          imgData.data[pixelNum] = parseInt(pixelCol.substr(0,2), 16); // not performant, i know!
+          imgData.data[pixelNum + 1] = parseInt(pixelCol.substr(2,2), 16);
+          imgData.data[pixelNum + 2] = parseInt(pixelCol.substr(4,2), 16);
+          imgData.data[pixelNum + 3] = 255; // A
+          pixelNum += 4;
+        }
+      }
+      
+    }
+    f.idx = 0;
+    f.ctx.putImageData(imgData, 0, 0);
+
+    if (dbg_DIM) dbg_DIM.innerText = imgData.width +'x'+ imgData.height;
+  }
+  function save() {
+    var name = dataselect.value
+    if (name.indexOf('bw-sfx') > 0) return // early exit
+    name = name+'_'+f.canv.width+'.png';
+    
+    var a = document.createElement("a");
+    var d = new Date();
+    d = d.toISOString();
+    // PNG
+    a.setAttribute('download', name);
+    var dataURL = f.canv.toDataURL('image/png');
+    var url = dataURL.replace(/^data:image\/png/,'data:application/octet-stream');
+    f.canv.toBlob(function(blob) {
+      var url = URL.createObjectURL(blob);
+      a.setAttribute('href', url);
+      a.click();
+      log('Screenshot saved as: '+ name);
+    });
+  }
+  function readPixelBlock(f) {
+    return [f.data[f.idx++], f.data[f.idx++], f.data[f.idx++], f.data[f.idx++], f.data[f.idx++], f.data[f.idx++], f.data[f.idx++], f.data[f.idx++] ];
+  }
+  // https://github.com/HoraceAndTheSpider/Bloodwych-68k/wiki/Atari-ST-Raw-Data-Format
+  // p = one pixelblock of 16 pixels = 4width x 4height x 4planes = 64 bits = 8 bytes
+  // n = selected pixelnumber
+  function get_atari_pixelcolor(p, n) {
+    switch (n) {
+      case  0: return ((p[6] & 0x80) >> 4) | ((p[4] & 0x80) >> 5) | ((p[2] & 0x80) >> 6) | ((p[0] & 0x80) >> 7);
+      case  1: return ((p[6] & 0x40) >> 3) | ((p[4] & 0x40) >> 4) | ((p[2] & 0x40) >> 5) | ((p[0] & 0x40) >> 6);
+      case  2: return ((p[6] & 0x20) >> 2) | ((p[4] & 0x20) >> 3) | ((p[2] & 0x20) >> 4) | ((p[0] & 0x20) >> 5);
+      case  3: return ((p[6] & 0x10) >> 1) | ((p[4] & 0x10) >> 2) | ((p[2] & 0x10) >> 3) | ((p[0] & 0x10) >> 4);
+      case  4: return ((p[6] &  0x8)     ) | ((p[4] &  0x8) >> 1) | ((p[2] &  0x8) >> 2) | ((p[0] &  0x8) >> 3);
+      case  5: return ((p[6] &  0x4) << 1) | ((p[4] &  0x4)     ) | ((p[2] &  0x4) >> 1) | ((p[0] &  0x4) >> 2);
+      case  6: return ((p[6] &  0x2) << 2) | ((p[4] &  0x2) << 1) | ((p[2] &  0x2)     ) | ((p[0] &  0x2) >> 1);
+      case  7: return ((p[6] &  0x1) << 3) | ((p[4] &  0x1) << 2) | ((p[2] &  0x1) << 1) | ((p[0] &  0x1)     );
+      case  8: return ((p[7] & 0x80) >> 4) | ((p[5] & 0x80) >> 5) | ((p[3] & 0x80) >> 6) | ((p[1] & 0x80) >> 7);
+      case  9: return ((p[7] & 0x40) >> 3) | ((p[5] & 0x40) >> 4) | ((p[3] & 0x40) >> 5) | ((p[1] & 0x40) >> 6);
+      case 10: return ((p[7] & 0x20) >> 2) | ((p[5] & 0x20) >> 3) | ((p[3] & 0x20) >> 4) | ((p[1] & 0x20) >> 5);
+      case 11: return ((p[7] & 0x10) >> 1) | ((p[5] & 0x10) >> 2) | ((p[3] & 0x10) >> 3) | ((p[1] & 0x10) >> 4);
+      case 12: return ((p[7] &  0x8)     ) | ((p[5] &  0x8) >> 1) | ((p[3] &  0x8) >> 2) | ((p[1] &  0x8) >> 3);
+      case 13: return ((p[7] &  0x4) << 1) | ((p[5] &  0x4)     ) | ((p[3] &  0x4) >> 1) | ((p[1] &  0x4) >> 2);
+      case 14: return ((p[7] &  0x2) << 2) | ((p[5] &  0x2) << 1) | ((p[3] &  0x2)     ) | ((p[1] &  0x2) >> 1);
+      case 15: return ((p[7] &  0x1) << 3) | ((p[5] &  0x1) << 2) | ((p[3] &  0x1) << 1) | ((p[1] &  0x1)     );
+    }
+    return 0;
+  }
+
+  //
+  // Public
+  //
+  my.load = load;
+  my.render = render;
+  my.save = save;
+
+  //
+  // Exit
+  //
+  return my;
+}(BWGFX || {}));
